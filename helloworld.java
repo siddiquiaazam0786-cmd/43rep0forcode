bd07009f2581c6d8544a2922bf0fc4421067665e
siddiquiaazam0786-cmd <siddiquiaazam0786@gmail.com>
2026-04-28 12:26:32 +0530

    Update greeting messages in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch43 team with aazam, Mohit, Yogesh , Sachoin");
-      System.out.println("Hello Aazam batch43 WITH FOR STUDENTS RUNNING in front of me and Anurag Prince in making CI automated vikas teach devops CI proecss");
+     System.out.println("Hello batch43 team with Aazam, Nitesh,  Harshal, Mohit, Yogesh , Sachoin");
+      System.out.println("Hello Aazam batch43 WITH FOR STUDENTS RUNNING in front of me and Harshal Anurag Prince in making CI automated vikas teach devops CI proecss");
     }  
 } 
